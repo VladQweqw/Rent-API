ad4ef10d6f2c721beef2d7dd84e1577a4c35e28f
Vlad <vladpoienariu@gmail.com>
2025-03-02 13:11:55 +0200

    first commit initializiton

---
+++ src/test/java/com/example/rent_api/RentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rent_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
