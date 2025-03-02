88d2f3e08a2e6f1ccd06ab3a59a18e999b13913d
Vlad <vladpoienariu@gmail.com>
2025-03-02 13:11:55 +0200

    first commit initializiton

---
+++ src/main/java/com/example/rent_api/RentApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.rent_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentApiApplication.class, args);
+	}
+
+}
